3277438b00630f4c42e552ae3cd25adebbfcc60a
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-12-16 10:05:51 +0100

    coding workouts.js

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const Workout = require('../models/Workout');
+const auth = require('../middleware/auth');
 
+const router = express.Router();
+
+router.post('/', auth, async (rq, rs) => {
+    const workout = new Workout({ ...rq.body, userId: rq.user.id });
+    await workout.save();
+    res.status(201).send('Workout logged');
+};
+
+router.get('/', auth, async (rq, rs) => {
+    const workouts = await Workout.find({ userId: req.user.id });
+    res.json(workouts);
+};
+
+module.exports = roter;
