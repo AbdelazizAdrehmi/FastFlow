eb69230f4e68cd28b2ea0f032777e4361cbe7d25
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-12-16 10:04:35 +0100

    coding auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,1 +1,27 @@
+const express = require('express');
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const bcrypt = require('bcrypt');
 
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 0);
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+    res.status(201).send('User  created');
+};
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(401).send('Invalid credentials');
+    }
+    const token = jwt.sign({ id: user.id }, config.JWT_SECRET);
+    res.json({ token });
+};
+
+module.exports = router;
