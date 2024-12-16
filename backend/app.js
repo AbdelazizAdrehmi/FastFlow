8dff27e3b7feb9fb117571fa63f13db8044ba9f2
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-12-16 10:10:50 +0100

    re-code app.js

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
 const workoutRoutes = require('./routes/workouts');
-const config = require('./confi');
+const config = require('./config');
 
 const app = express();
 
@@ -10,12 +10,12 @@ app.use(express.json());
 
 mongoose.connect(config.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
-    .catch(err => console..error(err));
+    .catch(err => console.error(err));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/workouts', workoutRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () ={
-    console.log(`Serverrunning on port ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
