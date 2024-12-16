f4e5e03dd8e51be1a4b03d648d533b5f66bd5a0a
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-12-16 10:07:26 +0100

    coding auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,1 +1,14 @@
+const config = require('../config');
 
+module.exports = (rq, res, next) => {
+    const token = rq.header('Authorization')?.replace('Bearer ', '');
+    if (!token) return res.status(400).send('Access denied');
+
+    try {
+        const verfied = jwt.verify(token, config.JWT_SECRET);
+        req.user = verified;
+        next();
+    } catch (err) {
+        res.status(400).send('invalid token');
+    }
+};
