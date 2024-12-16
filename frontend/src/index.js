87e4ceaf68ba8b01b8001c1b24ed881e2f46bae6
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-12-16 14:32:27 +0100

    coding index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles.css'; // Importing styles
 
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
