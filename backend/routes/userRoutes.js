fe00bf5387062a46b3952c15975cd229c857406c
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2025-01-20 17:00:26 +0100

    Update userRoutes.js with full comments

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,29 +1,11 @@
-const express = require('express');
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
-const config = require('../config');
-const bcrypt = require('bcrypt');
+const express = require('express'); // Import ```javascript
+const router = express.Router(); // Creating a new router
+const { registerUser , loginUser  } = require('../controllers/userController'); // Importing user controller functions
 
-const router = express.Router();
+// Route for user registration
+router.post('/register', registerUser ); // POST request to register a new user
 
-// User signup route
-router.post('/signup', async (req, res) => {
-    const { username, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ username, password: hashedPassword });
-    await user.save();
-    res.status(201).send('User  created');
-});
+// Route for user login
+router.post('/login', loginUser ); // POST request for user login
 
-// User login route
-router.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-        return res.status(401).send('Invalid credentials');
-    }
-    const token = jwt.sign({ id: user._id }, config.JWT_SECRET);
-    res.json({ token });
-});
-
-module.exports = router;
+module.exports = router; // Exporting user routes
