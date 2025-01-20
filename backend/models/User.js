8256f65b4b101a03784de90b7af6a761df546b35
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2025-01-20 16:57:46 +0100

    Update User.js with full comments

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); // Importing Mongoose
 
 // User schema definition
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
 });
 
-// Export the User model
-module.exports = mongoose.model('User ', UserSchema);
+// User model
+const User = mongoose.model('User ', userSchema);
+
+module.exports = User; // Exporting User model
