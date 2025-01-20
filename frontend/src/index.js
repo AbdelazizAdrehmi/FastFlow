9314034adf1155208b54b58cf85f9e85a8e5857d
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2025-01-20 15:28:43 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './styles.css'; // Importing styles
+import React from 'react'; // Importing React library
+import ReactDOM from 'react-dom'; // Importing ReactDOM for rendering
+import App from './App'; // Importing the main App component
+import './styles/App.css'; // Importing global styles
 
+// Rendering the App component into the root div
 ReactDOM.render(
     <React.StrictMode>
         <App />
