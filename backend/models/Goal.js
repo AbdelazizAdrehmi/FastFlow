7b93bb32721453ce8873d8469d9c38a9577fa217
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2025-01-20 16:58:31 +0100

    Update Goal.js with full comments

--- backend/models/Goal.js
+++ backend/models/Goal.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose'); // Importing Mongoose
 
+// Goal schema definition
+const goalSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User ', required: true },
+    steps: { type: Number, required: true }, // Daily steps goal
+    calories: { type: Number, required: true }, // Calories to burn goal
+});
+
+// Goal model
+const Goal = mongoose.model('Goal', goalSchema);
+
+module.exports = Goal; // Exporting Goal model
